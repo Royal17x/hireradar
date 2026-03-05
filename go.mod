b6f2d722949581c360ea2b7481bd8d95f9c20bef
divergence <kurose245@gmail.com>
2026-03-05 23:26:10 +0300

    test: unit test for vacancy usecase, all 5 scenarios

--- go.mod
+++ go.mod
@@ -8,19 +8,23 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/pressly/goose/v3 v3.27.0
 	github.com/redis/go-redis/v9 v9.18.0
+	github.com/stretchr/testify v1.11.1
 )
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.6.1 // indirect
 	github.com/sethvargo/go-retry v0.3.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
---
+++ internal/usecase/vacancy_test.go
@@ -0,0 +1,93 @@
+package usecase
+
+import (
+	"context"
+	"errors"
+	"github.com/Royal17x/hireradar/internal/domain"
+	"github.com/Royal17x/hireradar/internal/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+func TestFetch(t *testing.T) {
+	tests := []struct {
+		name          string
+		hhID          string
+		wantSaveCalls int
+		isSeen        bool
+		wantErr       bool
+		fetcherErr    error
+		saveErr       error
+		isSeenErr     error
+	}{
+		{
+			name:       "ошибка fetcher",
+			fetcherErr: errors.New("ошибка клиента"),
+			wantErr:    true,
+		},
+		{
+			name:          "ошибка сохранения в БД",
+			saveErr:       errors.New("ошибка БД"),
+			wantSaveCalls: 1,
+			wantErr:       true,
+		},
+		{
+			name:    "вакансия уже была сохранена",
+			hhID:    "123",
+			isSeen:  true,
+			wantErr: false,
+		},
+		{
+			name:          "новая вакансия пришла из парсинга",
+			hhID:          "123",
+			wantSaveCalls: 1,
+			isSeen:        false,
+			wantErr:       false,
+		},
+		{
+			name:      "ошибка проверки вакансии из существующих",
+			hhID:      "123",
+			isSeenErr: errors.New("ошибка БД"),
+			wantErr:   true,
+		},
+	}
+
+	// arrange
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			vacancyRepo := mocks.NewVacancyRepository(t)
+			cacheRepo := mocks.NewCacheRepository(t)
+			fetcher := mocks.NewVacancyFetcher(t)
+
+			fetcher.On("FetchVacancies", mock.Anything, "golang").
+				Return([]domain.Vacancy{{HhID: tt.hhID}}, tt.fetcherErr)
+			if tt.fetcherErr == nil {
+				cacheRepo.On("IsSeen", mock.Anything, tt.hhID).
+					Return(tt.isSeen, tt.isSeenErr)
+			}
+			if tt.wantSaveCalls > 0 && !tt.isSeen && tt.isSeenErr == nil {
+				vacancyRepo.On("Save", mock.Anything, mock.Anything).
+					Return(tt.saveErr)
+				if tt.saveErr == nil {
+					cacheRepo.On("SetSeen", mock.Anything, tt.hhID).
+						Return(tt.saveErr)
+				}
+
+			}
+
+			// act
+			ucase := NewVacancyUsecase(vacancyRepo, cacheRepo, fetcher)
+			err := ucase.FetchAndStore(context.Background(), "golang")
+
+			// assert
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+
+		})
+	}
+
+}
